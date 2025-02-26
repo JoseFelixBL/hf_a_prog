e7e679c6dfbb5371c2fdce1cce53160c8a98601e
José Bello <jfbello.work@gmail.com>
2025-02-26 19:17:25 +0100

    Ch1- game.py with hints - elif

--- ch-01/game.py
+++ ch-01/game.py
@@ -3,6 +3,10 @@ g = input("Guess the number: ")
 guess = int(g)
 if guess == 5:
     print("You win!")
+elif guess > 5:
+    print("Too high!")
+    print("You lose!")
 else:
+    print("Too low!")
     print("You lose!")
 print("Game over!")
