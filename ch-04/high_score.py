e161f22dced9c86db8f5f7e7bdd233f66d1bff99
José Bello <jfbello.work@gmail.com>
2025-02-27 12:06:54 +0100

    Ch5- Switch key,values in dict to sorted() them by key-score

--- ch-04/high_score.py
+++ ch-04/high_score.py
@@ -4,7 +4,10 @@ scores = {}
 with open("results.txt") as result_f:
     for line in result_f:
         name, score = line.split()
-        scores[name] = float(score)
+        scores[score] = name
 
-for name, score in scores.items():
-    print(f"{name}\t{score}")
+# for name, score in scores.items():
+#     print(f"{score}\t{name}")
+
+for ord_score in sorted(scores.keys(), reverse=True):
+    print(f"{ord_score}\t{scores[ord_score]}")
