81231f9ef4e35c0701564d05e97ded989fa21661
José Bello <jfbello.work@gmail.com>
2025-02-27 11:25:13 +0100

    Ch4- high_score.py using _with_ on the open() function

--- ch-04/high_score.py
+++ ch-04/high_score.py
@@ -1,10 +1,10 @@
 highest_score = 0
 winner_name = ""
-result_f = open("results.txt")
-for line in result_f:
-    name, score = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
-        winner_name = name
-result_f.close()
+with open("results.txt") as result_f:
+    for line in result_f:
+        name, score = line.split()
+        if float(score) > highest_score:
+            highest_score = float(score)
+            winner_name = name
+# result_f.close()
 print(f"The highest score was: {highest_score} by {winner_name}")
