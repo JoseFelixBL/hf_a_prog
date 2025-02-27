07ca515afde90854cfc08eb6c5deafa94cb24ce3
José Bello <jfbello.work@gmail.com>
2025-02-27 11:11:44 +0100

    Ch4- add names to results.txt, add split and multiple assignement to highest_score.py

--- ch-04/high_score.py
+++ ch-04/high_score.py
@@ -1,7 +1,10 @@
 highest_score = 0
+winner_name = ""
 result_f = open("results.txt")
 for line in result_f:
-    if float(line) > highest_score:
-        highest_score = float(line)
+    name, score = line.split()
+    if float(score) > highest_score:
+        highest_score = float(score)
+        winner_name = name
 result_f.close()
-print(f"The highest score was: {highest_score}")
+print(f"The highest score was: {highest_score} by {winner_name}")
