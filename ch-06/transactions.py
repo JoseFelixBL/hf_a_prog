46e7d756fd561389cfd2bdc0fa385c5709e4e301
José Bello <jfbello.work@gmail.com>
2025-02-28 11:13:50 +0100

    Ch6- transactions.py corrected description format

--- ch-06/transactions.py
+++ ch-06/transactions.py
@@ -6,4 +6,4 @@ def save_transaction(price, credit_card, description):
     output_dir = Path('ch-06')
     output_final = output_dir / output_file
     with open(output_final, "a") as file:
-        file.write(f"{int(price*100):07}{credit_card:16}{description:16}\n")
+        file.write(f"{int(price*100):07}{credit_card:16}{description:>16}\n")
