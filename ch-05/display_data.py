45e109b3c41e776b924d02a2bdca074d1304d5d1
José Bello <jfbello.work@gmail.com>
2025-02-28 10:14:23 +0100

    Ch5- display_data.py, modify find_details(id2find) to search in sqlite3 DB surferssDB.sdb, added ch-05 Path to surfersDB.sdb

--- ch-05/display_data.py
+++ ch-05/display_data.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import sqlite3
 
 
-def find_details(id):
+def find_details_in_csv_file(id):
     file = Path('rsa_data.csv')
     dir = Path('ch-05')
     file_path = dir / file
@@ -13,6 +14,35 @@ def find_details(id):
             if line.startswith(str(id)):
                 (s['id'], s['name'], s['country'], s['average'],
                  s['board'], s['age']) = line.strip().split(';')
+    return s
+
+
+def find_details(id2find):
+    # Ready baked code
+    file = Path('surfersDB.sdb')
+    dir = Path('ch-05')
+    file_path = dir / file
+
+    db = sqlite3.connect(file_path)
+
+    db.row_factory = sqlite3.Row
+
+    cursor = db.cursor()
+    cursor.execute("select * from surfers")
+    rows = cursor.fetchall()
+
+    s = {}
+    s['id'] = None
+
+    for row in rows:
+        if row['id'] == id2find:
+            s['id'] = str(row['id'])
+            s['name'] = row['name']
+            s['country'] = row['country']
+            s['average'] = str(row['average'])
+            s['board'] = row['board']
+            s['age'] = str(row['age'])
+            cursor.close()
     return s
 
 
