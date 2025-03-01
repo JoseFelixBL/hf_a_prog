136aadb84c75c592eaa88c33f1cd172178433bdc
José Bello <jfbello.work@gmail.com>
2025-03-01 08:50:33 +0100

    Ch8- head_ex_deliveries.py GUI and store data in deliveries.txt

---
+++ ch-08/head_ex_deliveries.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from pathlib import Path
+
+app = Tk()
+app.title("Head-Ex Deliveries")
+
+Label(app, text="Depot:").pack()
+
+depot = Entry(app)
+depot.pack()
+
+description = Entry(app)
+description.pack()
+
+address = Text(app)
+address.pack()
+
+
+def save_data():
+    dir = Path('ch-08')
+    with open(dir / "deliveries.txt", "a") as file:
+        file.write("Depot:\n")
+        file.write(f"{depot.get()}\n")
+        file.write("Description:\n")
+        file.write(f"{description.get()}\n")
+        file.write("Address:\n")
+        file.write(f"{address.get('1.0', END)}\n")
+        depot.delete(0, END)
+        description.delete(0, END)
+        address.delete('1.0', END)
+
+
+Button(app, text="Save", command=save_data).pack()
+
+app.mainloop()
