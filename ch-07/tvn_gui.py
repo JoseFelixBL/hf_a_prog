1d644cdbbb76d9732884a0814e0d71a6c2c197ca
José Bello <jfbello.work@gmail.com>
2025-03-01 08:09:39 +0100

    Ch7- ttvn_gui.py, Add a button that writes on terminal

--- ch-07/tvn_gui.py
+++ ch-07/tvn_gui.py
@@ -1,6 +1,15 @@
 from tkinter import *
 
+
+def tell_it():
+    print("Hello, world!")
+
+
 app = Tk()
 app.title("Your TKInter app")
 app.geometry('450x100+200+100')
+b1 = Button(app, text="Click me!", width=10, command=tell_it)
+b1.pack()
+
+
 app.mainloop()
