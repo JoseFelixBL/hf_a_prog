c1b52c5aab2017dbabf5fb97293e0e1326560377
José Bello <jfbello.work@gmail.com>
2025-03-01 09:52:55 +0100

    Ch8.5- Exception handler on save_data(), print to terminal

--- ch-08/head_ex_deliveries.py
+++ ch-08/head_ex_deliveries.py
@@ -1,13 +1,40 @@
 from tkinter import *
 from pathlib import Path
 
+
+def save_data():
+    dir = Path('ch-08')
+    try:
+        with open(dir / "deliveries.txt", "a") as file:
+            file.write("Depot:\n")
+            file.write(f"{depot.get()}\n")
+            file.write("Description:\n")
+            file.write(f"{description.get()}\n")
+            file.write("Address:\n")
+            file.write(f"{address.get('1.0', END)}\n")
+            depot.set(None)
+            description.delete(0, END)
+            address.delete('1.0', END)
+    except Exception as ex:
+        print(f"An error occurred: {ex}")
+
+
+def read_depots(file):
+    dir = Path('ch-08')
+    depots = []
+    with open(dir / file, "r") as file:
+        for line in file:
+            depots.append(line.strip())
+    return depots
+
+
 app = Tk()
 app.title("Head-Ex Deliveries")
 
 Label(app, text="Depot:").pack()
 depot = StringVar()
 depot.set(None)
-depots = ["Cambridge, MA", "Cambridge, UK", "Seattle, WA"]
+depots = read_depots("depots.txt")
 OptionMenu(app, depot, *depots).pack()
 
 Label(app, text="Description:").pack()
@@ -19,20 +46,6 @@ Label(app, text="Address:").pack()
 
 address = Text(app)
 address.pack()
-
-
-def save_data():
-    dir = Path('ch-08')
-    with open(dir / "deliveries.txt", "a") as file:
-        file.write("Depot:\n")
-        file.write(f"{depot.get()}\n")
-        file.write("Description:\n")
-        file.write(f"{description.get()}\n")
-        file.write("Address:\n")
-        file.write(f"{address.get('1.0', END)}\n")
-        depot.set(None)
-        description.delete(0, END)
-        address.delete('1.0', END)
 
 
 Button(app, text="Save", command=save_data).pack()
