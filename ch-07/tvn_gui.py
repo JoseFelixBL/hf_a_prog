e66b2c196580a2c790e42834a6a95083fb87fd36
José Bello <jfbello.work@gmail.com>
2025-03-01 08:16:30 +0100

    Ch7- tvn_gui.py, correct and wrong buttons with count

--- ch-07/tvn_gui.py
+++ ch-07/tvn_gui.py
@@ -1,15 +1,32 @@
 from tkinter import *
 
+number_correct = 0
+number_wrong = 0
 
-def tell_it():
-    print("Hello, world!")
+
+def play_correct_sound():
+    global number_correct
+    number_correct += 1
+
+
+def play_wrong_sound():
+    global number_wrong
+    number_wrong += 1
 
 
 app = Tk()
-app.title("Your TKInter app")
-app.geometry('450x100+200+100')
-b1 = Button(app, text="Click me!", width=10, command=tell_it)
-b1.pack()
+app.title("TVN Game Show")
+app.geometry('300x100+200+100')
+
+b1 = Button(app, text="Correct!", width=10, command=play_correct_sound)
+b1.pack(side="left", padx=10, pady=10)
+
+b2 = Button(app, text="Wrong!", width=10, command=play_wrong_sound)
+b2.pack(side="right", padx=10, pady=10)
 
 
 app.mainloop()
+
+print(f"Number correct: {number_correct}")
+print(f"Number wrong: {number_wrong}")
+print(f'Total answers: {number_correct + number_wrong}')
